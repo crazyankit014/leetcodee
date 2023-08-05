aa51d251a2b711c7879b45e88dcf090632c258ae
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-08-05 12:12:12 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (58.54%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -12,17 +12,45 @@
 class Solution {
 public:
 
- void solve(TreeNode* root, int level ,vector<int>& node)
- {
-     if(root==NULL) return ;
-     if(node.size()==level) node.push_back(root->val);
-     solve(root->right,level+1,node);
-     solve(root->left,level+1,node);
- }
+//  void solve(TreeNode* root, int level ,vector<int>& node)
+//  {
+//      if(root==NULL) return ;
+//      if(node.size()==level) node.push_back(root->val);
+//      solve(root->right,level+1,node);
+//      solve(root->left,level+1,node);
+//  }
     vector<int> rightSideView(TreeNode* root) {
-    vector<int>node;
+    // vector<int>node;
 
-    solve(root,0,node);
-    return node;
+    // solve(root,0,node);
+    // return node;
+
+    if(root==NULL) return {};
+     vector<int>view;
+    queue<TreeNode*>q;
+    q.push(root);
+   while(!q.empty())
+   {
+       int n=q.size();
+       for(int i=0;i<n;i++)
+       {
+           TreeNode* node=q.front();
+           q.pop();
+           if(i==n-1)
+           {
+               view.push_back(node->val);
+           }
+
+           if(node->left)
+           {
+               q.push(node->left);
+           }
+           if(node->right)
+           {
+               q.push(node->right);
+           }
+       }
+   }
+   return view;
     }
 };
