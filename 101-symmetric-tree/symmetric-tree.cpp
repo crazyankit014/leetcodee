f1cbf1894fdb01fe07a2290b1d38bf9905f70f7c
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-08-05 13:09:03 +0530

    Time: 3 ms (85.78%) | Memory: 16.7 MB (6.15%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -12,15 +12,30 @@
 class Solution {
 public:
 
-   bool solve (TreeNode* left , TreeNode* right)
-   {
-       if(left==NULL || right ==NULL) return left==right;
-
-       if(left->val != right->val) return false;
-
-       return  solve(left->left, right->right) && solve(left->right,right->left);
-   }
+//    bool solve (TreeNode* left , TreeNode* right)
+//    {
+//        if(left==NULL || right ==NULL) return left==right;
+//        if(left->val != right->val) return false;
+//        return  solve(left->left, right->right) && solve(left->right,right->left);
+//    }
     bool isSymmetric(TreeNode* root) {
-        return root==NULL || solve(root->left,root->right);
+        // return root==NULL || solve(root->left,root->right);
+
+        if(root==NULL) return true;
+
+        queue<pair<TreeNode*, TreeNode*>> q;
+        q.push({root->left, root->right});
+        while(!q.empty())
+        {
+           pair<TreeNode*, TreeNode*> node = q.front();
+            q.pop();
+            if(!node.first && !node.second) continue;
+            if(!node.first || !node.second) return false;
+            if(node.first->val != node.second->val) return false;
+
+            q.push({node.first->left, node.second->right});
+            q.push({node.first->right, node.second->left});
+        }
+        return true;
     }
 };
