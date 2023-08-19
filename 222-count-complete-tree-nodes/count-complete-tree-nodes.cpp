46127a6e0cde369ab8642e5046a718966e619332
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-08-19 08:29:25 +0530

    Time: 19 ms (96.95%) | Memory: 30.8 MB (36.07%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -11,30 +11,35 @@
  */
 class Solution {
 public:
-    int LeftHeight(TreeNode* root)
-    {
-        if(root==NULL) return 0;
+    // int LeftHeight(TreeNode* root)
+    // {
+    //     if(root==NULL) return 0;
 
-        return LeftHeight(root->left)+1;
-    }
-     int RightHeight(TreeNode* root)
-    {
-        if(root==NULL) return 0;
+    //     return LeftHeight(root->left)+1;
+    // }
+    //  int RightHeight(TreeNode* root)
+    // {
+    //     if(root==NULL) return 0;
 
-        return RightHeight(root->right)+1;
-    }
+    //     return RightHeight(root->right)+1;
+    // }
 
 
     int countNodes(TreeNode* root) {
+        // if(root==NULL) return 0;
+
+        // int lh=LeftHeight(root);
+        // int rh=RightHeight(root);
+        // if(lh==rh)
+        // {
+        //     return pow(2,lh)-1;
+        // }
+        // return countNodes(root->left)+ countNodes(root->right)+1;
+      
         if(root==NULL) return 0;
 
-        int lh=LeftHeight(root);
-        int rh=RightHeight(root);
-        if(lh==rh)
-        {
-            return pow(2,lh)-1;
-        }
-        return countNodes(root->left)+ countNodes(root->right)+1;
+        return 1+ countNodes(root->left)+countNodes(root->right);
 
+       
     }
 };
