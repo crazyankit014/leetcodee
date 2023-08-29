be61a207b9b91c0d79d36d6d81f1565b2ab2375a
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-08-30 01:26:03 +0530

    Time: 817 ms (53.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,7 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    SET N=N-1;
+  RETURN (
+      select distinct(salary) from employee order by salary desc limit 1 offset N
+  );
+END
