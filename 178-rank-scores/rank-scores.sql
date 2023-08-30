91af2234d869b3bdf55def6c35f6396b1d7f68f3
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-08-30 10:53:49 +0530

    Time: 1305 ms (7.13%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,4 +1,12 @@
-SELECT score, 
-       DENSE_RANK() OVER(ORDER BY score DESC) AS `rank`
-FROM Scores
+# SELECT score, 
+#        DENSE_RANK() OVER(ORDER BY score DESC) AS `rank`
+# FROM Scores
+# ORDER BY score DESC;
+
+SELECT score,
+       (SELECT COUNT(DISTINCT score)
+        FROM Scores
+        WHERE score >= s.score) AS `rank`
+FROM Scores s
 ORDER BY score DESC;
+
