92b1ed63064b5af28918e8e48c1aed9cae4d7959
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-11-24 10:46:45 +0530

    Time: 91 ms (65.96%) | Memory: 53.7 MB (54.72%) - LeetSync

--- 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.cpp
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int maxCoins(vector<int>& piles) {
-        int n=piles.size();
-        sort(piles.begin(),piles.end());
-        int  a=piles.size()-1;
-        int m=piles.size()-2;
-        int b=0;
-        int result=0;
-        while(m>b)
-        {
-           result=result+piles[m];
-           b++;
-           a=a-2;   // not needed
-           m=m-2;
-        }
+
+         // Approach 1 tc (nlogn)
+
+        // int n=piles.size();
+        // sort(piles.begin(),piles.end());
+        // int  a=n-1;     // largest 
+        // int m=n-2;      // second largest 
+        // int b=0;         // smallest
+        // int result=0;
+        // while(m>b)
+        // {
+        //    result=result+piles[m];
+        //    b++;
+        //    a=a-2;   // not needed
+        //    m=m-2;
+        // }
+        // return result;
+
+
+        // approach 2 tc(nlogn)
+
+         int n=piles.size();
+         sort(piles.begin(),piles.end());
+         int result=0;
+
+         for(int i=n/3;i<n;i=i+2)
+         {
+             result=result+piles[i];
+         }
+
         return result;
     }
 };
