2697d6bb3271b1afea478e0bdfac3b018b87eef5
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-11-29 08:57:44 +0530

    Time: 4 ms (30.61%) | Memory: 6.4 MB (57.22%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -2,12 +2,18 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int count=0;
-        for(int i=31;i>=0;i--)
+        // for(int i=31;i>=0;i--)
+        // {
+        //     if(n>>i & 1 == 1)
+        //     {
+        //         count++;
+        //     }
+        // }
+        while(n>0)
         {
-            if(n>>i & 1 == 1)
-            {
+             n=n & (n-1);
                 count++;
-            }
+            
         }
         return count;
     }
