59cb839daaf81765b20fe6fa093428df211b448a
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-12-01 19:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (5.94%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        string s1 = "";
-        string s2 = "";
-
-        for(const string& s : word1)
-            s1 += s;
-        for(const string& s : word2)
-            s2 += s;
-
-        return s1==s2;
+        string a,b;
+        for(int i=0;i<word1.size();i++){
+            a=a+word1[i];
+        }
+        for(int i=0;i<word2.size();i++){
+            b=b+word2[i];
+        }
+        return a==b;
     }
 };
