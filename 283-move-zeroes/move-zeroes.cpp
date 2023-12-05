05a27bbc30af3ffe38cf471e0fb42877a8d6ef0b
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-12-05 19:48:27 +0530

    Time: 21 ms (29.24%) | Memory: 19.5 MB (65.36%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int>temp;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
+        // vector<int>temp;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i] !=0)
+        //     {
+        //         temp.push_back(nums[i]);
+        //     }
+        // }
+            
+        //     int m=temp.size();
+        //     for(int i=0;i<m;i++)
+        //     {
+        //         nums[i]=temp[i];
+        //     }
+
+        //     for(int i=m;i<n;i++)
+        //     {
+        //         nums[i]=0;
+        //     }
+
+        int i=0;
+        for(int j=0;j<nums.size();j++)
         {
-            if(nums[i] !=0)
+            if(nums[j] != 0)
             {
-                temp.push_back(nums[i]);
+                swap(nums[i],nums[j]);
+                i++;
             }
         }
-            
-            int m=temp.size();
-            for(int i=0;i<m;i++)
-            {
-                nums[i]=temp[i];
-            }
-
-            for(int i=m;i<n;i++)
-            {
-                nums[i]=0;
-            }
     }
 };
