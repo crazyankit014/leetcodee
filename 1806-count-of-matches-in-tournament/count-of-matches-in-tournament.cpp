f683e3bfd682abdf449b1a977450cf8bbb717d38
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-12-05 20:03:49 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (73.09%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
+       // first approach 
         return (n-1);
+
+        // 2nd approach
+
+        int matches=0;
+        while(n>1)
+        {
+            if(n%2==0){
+           matches=matches+(n/2);
+            n=n/2;
+            }
+            else
+            {
+                matches=matches+(n-1)/2;
+                n=(n-1)/2+1;
+            }
+            
+        }
+        return matches;
     }
 };
