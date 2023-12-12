eeeb74c9a86630b892413aebcaf792ea5cc13e5c
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-12-12 09:20:46 +0530

    Time: 44 ms (37.98%) | Memory: 29.6 MB (80.77%) - LeetSync

--- 1372-check-if-it-is-a-good-array/check-if-it-is-a-good-array.cpp
+++ 1372-check-if-it-is-a-good-array/check-if-it-is-a-good-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
 
-int gcd(int a,int b){
-   while(a%b != 0) {
-       int rem=a%b;
-       a=b;
-       b=rem;
-   }
-   return b;
+// int gcd(int a,int b){
+//    while(a%b != 0) {
+//        int rem=a%b;
+//        a=b;
+//        b=rem;
+//    }
+//    return b;
 
-}
+// }
 
     bool isGoodArray(vector<int>& nums) {
         int n=nums.size();
@@ -17,8 +17,7 @@ int gcd(int a,int b){
         for(int i=0;i<n;i++)
         {
             a=gcd(a,nums[i]);
-            if(a==1) return true;
         }
-        return false;
+        return a==1;
     }
 };
