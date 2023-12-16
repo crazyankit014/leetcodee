2c7b8c0cd718a11b7ca49c105eb1238ec6a7d452
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-12-16 15:32:44 +0530

    Time: 16 ms (10.38%) | Memory: 7.7 MB (45.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n=s.length();
-        int m=t.length();
-        if(n != m) return false;
+        // int n=s.length();
+        // int m=t.length();
+        // if(n != m) return false;
 
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
