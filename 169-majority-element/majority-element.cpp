819dd9621c6462a5798d157fbcab3bc69e9da192
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-12-17 11:45:22 +0530

    Time: 12 ms (71.07%) | Memory: 20.2 MB (11.18%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,25 +4,42 @@ public:
         // sort(nums.begin(),nums.end());
         // int n=nums.size();
         // return nums[n/2];
-        int cnt=0;
-        int el;
+
+        //2nd approach
+
+        // int cnt=0;
+        // int el;
         int n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-            if(cnt==0)
-            {
-                cnt=1;
-                el=nums[i];
-            }
-            else if(el==nums[i])cnt++;
-            else cnt--;
-        }
-        int cnt1=0;
-        for(int  i=0;i<n;i++)
-        {
-            if(nums[i]==el) cnt1++;
-        }
-        if(cnt1>(n/2)) return el;
-        return -1;
-    }
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(cnt==0)
+        //     {
+        //         cnt=1;
+        //         el=nums[i];
+        //     }
+        //     else if(el==nums[i])cnt++;
+        //     else cnt--;
+        // }
+        // int cnt1=0;
+        // for(int  i=0;i<n;i++)
+        // {
+        //     if(nums[i]==el) cnt1++;
+        // }
+        // if(cnt1>(n/2)) return el;
+        // return -1;
+
+        // 3rd approach
+         unordered_map<int,int>mpp;
+         for(int i=0;i<n;i++)
+         {
+            mpp[nums[i]]++;
+         }
+
+         for(auto x: mpp)
+         {
+             if(x.second>n/2)
+               return x.first;
+         }
+             return -1;
+     }
 };
