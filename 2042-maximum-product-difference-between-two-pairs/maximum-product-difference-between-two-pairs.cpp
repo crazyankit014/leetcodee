e3fcf8f477e377340eb137c102845128bd9891a9
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-12-18 11:49:48 +0530

    Time: 22 ms (66.27%) | Memory: 20.6 MB (64.73%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
@@ -1,15 +1,49 @@
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+
+        // tc= nlogn , sc=  logn 
+
+        // sort(nums.begin(),nums.end());
+        // int n=nums.size();
+        // long long x;
+        // long long  y;
+        // for(int i=1;i<=n;i++)
+        // {
+        //    x=nums[0]*nums[1];
+        //    y=nums[n-1]*nums[n-2];
+        // }
+        // return y-x;
+
+        // 2nd approach 
+
+        int l=0;
+        int sl=0;
+        int s=INT_MAX;
+        int ss=INT_MAX;
         int n=nums.size();
-        long long x;
-        long long  y;
-        for(int i=1;i<=n;i++)
+        for(int i=0;i<n;i++)
         {
-           x=nums[0]*nums[1];
-           y=nums[n-1]*nums[n-2];
+           if(nums[i]>l)
+           {
+               sl=l;
+               l=nums[i];
+           }
+           else
+           {
+               sl=max(sl,nums[i]);
+           }
+
+           if(nums[i] < s)
+           {
+               ss=s;
+               s=nums[i];
+           }
+           else{
+               ss=min(ss,nums[i]);
+           }
         }
-        return y-x;
+
+        return l * sl - s*ss; 
     }
 };
