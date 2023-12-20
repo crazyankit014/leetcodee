347648536c6aabeefe631b5211351561020274b1
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-12-20 13:52:57 +0530

    Time: 24 ms (5.34%) | Memory: 47 MB (58.30%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,12 +1,40 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
+      // 1st approach tc(nlogn)
+
+        // int n=prices.size();
+        // sort(prices.begin(),prices.end());
+        // for(int i=0;i<n;i++){
+        //     if(prices[0]+prices[1] > money) 
+        //     return money;
+        // }
+        // return money-(prices[0]+prices[1]);
+
+        // 2nd approach o(n)
+
         int n=prices.size();
-        sort(prices.begin(),prices.end());
-        for(int i=0;i<n;i++){
-            if(prices[0]+prices[1] > money) 
-            return money;
+        int mini=INT_MAX;
+        int smini=INT_MAX;
+
+        for(int i=0;i<n;i++)
+        {
+            if(prices[i]<mini)
+            {
+                smini=mini;
+                mini=prices[i];
+            }
+            else
+            {
+                smini=min(prices[i],smini);
+            }
         }
-        return money-(prices[0]+prices[1]);
+      if(mini+smini > money)
+      {
+          return money;
+      }
+
+      return money-(mini+smini);
+
     }
 };
