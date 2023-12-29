a12ffc184d80eecf934765b544413dd7237f1650
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-12-29 17:19:18 +0530

    Time: 391 ms (5.03%) | Memory: 73.1 MB (16.93%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        int ans=0;
-        int i=0;
-        int j=n-1;
-        while(i<j)
-        {
-            if(nums[i]+nums[j]==k)
-            {
-                ans++;
-                i++;
-                j--;
-            }
-            else if(nums[i]+nums[j] < k)
-            {
-                i++;
-            }
-            else 
-            {
-                j--;
-            }
+    //     int n=nums.size();
+    //     sort(nums.begin(),nums.end());
+    //     int ans=0;
+    //     int i=0;
+    //     int j=n-1;
+    //     while(i<j)
+    //     {
+    //         if(nums[i]+nums[j]==k)
+    //         {
+    //             ans++;
+    //             i++;
+    //             j--;
+    //         }
+    //         else if(nums[i]+nums[j] < k)
+    //         {
+    //             i++;
+    //         }
+    //         else 
+    //         {
+    //             j--;
+    //         }
+    //     }
+    //    return ans;
+    std::unordered_map<int, int> freq;
+
+    int ans = 0;
+    for (int num : nums) {
+        int complement = k - num;
+        if (freq[complement] > 0) {
+            // If complement is found in the hashmap, decrement its frequency
+            freq[complement]--;
+            ans++;
+        } else {
+            // Increment the frequency of the current element
+            freq[num]++;
         }
-       return ans;
+    }
+
+    return ans;
     }
 };
