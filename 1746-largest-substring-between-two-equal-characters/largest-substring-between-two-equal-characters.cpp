c731ec5cbae1c9a1dcea7a475684a75bd4e695f9
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-12-31 11:26:03 +0530

    Time: 3 ms (47.38%) | Memory: 6.8 MB (41.15%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-        int ans= -1;
+        // int ans= -1;
+        // for(int i=0;i<s.size();i++)
+        // {
+        //     for(int j=i+1;j<s.size();j++)
+        //     {
+        //         if(s[i] == s[j])
+        //         {
+        //             ans=max(ans,j-i-1);
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        unordered_map<char,int> mpp;
+        int ans=-1;
         for(int i=0;i<s.size();i++)
         {
-            for(int j=i+1;j<s.size();j++)
+    
+            if(mpp.find(s[i]) == mpp.end()){
+                mpp[s[i]]=i;
+            }
+
+            else
             {
-                if(s[i] == s[j])
-                {
-                    ans=max(ans,j-i-1);
-                }
+                ans=max(ans,i-mpp[s[i]]-1);
             }
         }
-        return ans;
-
+                 return ans;
     }
 };
