58d4c244514173b5f4e062cd0af2a0d2d7fd664e
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2024-01-03 17:17:09 +0530

    Time: 6 ms (49.21%) | Memory: 14.1 MB (80.77%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+       int sum=1;
+        int maxi=nums[0];
+        for(int i=0;i<nums.size();i++){
+            sum=sum*nums[i];
+            maxi=max(sum,maxi);
+            if(sum==0)
+                sum=1;
+        }
+        sum=1;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            sum=sum*nums[i];
+            maxi=max(sum,maxi);
+            if(sum==0)
+            {
+                sum=1;
+            }
+        }
+        
+        return maxi;
+    }
+};
