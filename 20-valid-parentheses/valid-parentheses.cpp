61c4472b092736cc96ba8e3e4f08d15b96cccb37
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2024-01-08 22:47:34 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (37.69%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for(auto i : s)
+        {
+           if(i=='(' or i=='{' or i=='[') st.push(i);
+           else
+            {
+                if(st.empty() || (st.top()=='(' and i!=')') || (st.top()=='{' and i!='}') || (st.top()=='[' and i!=']')) return false;
+                st.pop(); 
+            }
+        }
+        return st.empty();
+           
+        }
+};
