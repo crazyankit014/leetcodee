2c87b13323f1c1815fc879485a5ef74dcfb6d4e9
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2024-01-17 14:20:25 +0530

    Time: 4 ms (47.45%) | Memory: 9.3 MB (9.43%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int>mpp;
+    //     unordered_map<int,int>mpp;
 
-        for(int i=0;i<arr.size();i++)
-        {
-            mpp[arr[i]]++;
-        }
-    unordered_set<int> st;
-    for (auto x : mpp) {
-        int freq=x.second;
-        if (st.find(freq) != st.end()) {
-            return false;
-        }
-            st.insert(freq);
+    //     for(int i=0;i<arr.size();i++)
+    //     {
+    //         mpp[arr[i]]++;
+    //     }
+    // unordered_set<int> st;
+    // for (auto x : mpp) {
+    //     int freq=x.second;
+    //     if (st.find(freq) != st.end()) {
+    //         return false;
+    //     }
+    //         st.insert(freq);
         
-    }
-    return true;
+    // }
+    // return true;
+
+       vector<int>vec(2001,0);
+       for(int i=0;i<arr.size();i++)
+       {
+           vec[arr[i]+1000]++;
+       }
+
+       sort(vec.begin(),vec.end());
+
+       for(int i=1;i<2001;i++)
+       {
+           if(vec[i] != 0 && vec[i] == vec[i-1])
+           {
+               return false;
+           }
+       }
+       return true;
     }
 };
