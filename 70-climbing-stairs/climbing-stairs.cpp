0067dea8b6797ddc061264c7656adcac891d266a
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2024-01-18 12:56:29 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (35.64%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int climbStairs(int n) {
-         vector<int> dp(n+1);
-        dp[0]=dp[1]=1;
-        for(int i=2;i<=n;i++)   
-            dp[i]=dp[i-1]+dp[i-2];
-        return dp[n];
+        //  vector<int> dp(n+1);
+        // dp[0]=dp[1]=1;
+        // for(int i=2;i<=n;i++)   
+        //     dp[i]=dp[i-1]+dp[i-2];
+        // return dp[n];
 
         // if(n<=2) return n;
 
@@ -20,5 +20,13 @@ public:
         //     m=k;
         // }
         // return k;
+         long long int prev1 = 1;
+        long long int prev2 = 1;
+        for(int i = 0; i < n; i++){
+            long long int temp = prev1;
+            prev1 = prev1 + prev2;
+            prev2 = temp;
+        }
+        return prev2;
     }
 };
