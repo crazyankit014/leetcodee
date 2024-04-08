9a760a27c4c1d76b6be8d3131b870d518f0ef129
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2024-04-08 23:03:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.29%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -3,25 +3,36 @@ public:
     void sortColors(vector<int>& nums) {
         // sort(nums.begin(),nums.end());
 
-        int n=nums.size();
-        int low=0,mid=0,high=n-1;
-        while(mid<=high)
-        {
-            if(nums[mid]==0){
-                swap(nums[low],nums[mid]);
-                low++;
-                mid++;
-            }
-            else if(nums[mid]==1)
-            {
-                mid++;
-            }
-            else
-            {
-                swap(nums[mid],nums[high]);
-                high--;
-            }
+        // int n=nums.size();
+        // int low=0,mid=0,high=n-1;
+        // while(mid<=high)
+        // {
+        //     if(nums[mid]==0){
+        //         swap(nums[low],nums[mid]);
+        //         low++;
+        //         mid++;
+        //     }
+        //     else if(nums[mid]==1)
+        //     {
+        //         mid++;
+        //     }
+        //     else
+        //     {
+        //         swap(nums[mid],nums[high]);
+        //         high--;
+        //     }
             
+        // }
+
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[j]<nums[i])
+                {
+                    swap(nums[i],nums[j]);
+                }
+            }
         }
     }
 };
